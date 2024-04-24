57894f28084dc7a21fd26e7e43a43110dcb656ff
Giteon <fox.gideon@gmail.com>
2024-04-24 02:14:09 -0700

    PHISHMOJIS

--- phish.js
+++ phish.js
@@ -1,18 +1,17 @@
 window.onload = function() {
-    const audio = document.querySelector('#phish-audio');  // Reference to the audio element
-    const phishButton = document.querySelector('#phish-button');  // Reference to the button
+    const audio = document.querySelector('#phish-audio'); 
+    const phishButton = document.querySelector('#phish-button'); 
     audio.volume = 0.3;
     // Function to toggle play/pause and update button text
     const togglePhish = () => {
         if (audio.paused) {  // If audio is paused, play it
             audio.play();
-            phishButton.textContent = 'STOP PHISHING';  // Change button text
+            phishButton.textContent = '✋ STOP PHISHING ✋';  
         } else {  // If audio is playing, pause it
             audio.pause();
-            phishButton.textContent = 'PHISH ME';  // Change button text
+            phishButton.textContent = '🎣 PHISH ME 🎣';  
         }
     };
 
-    // Set up the event listener to toggle play/pause on button click
     phishButton.addEventListener('click', togglePhish);
 };
