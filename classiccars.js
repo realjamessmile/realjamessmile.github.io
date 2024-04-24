f4d231c2ee5f9784f95660b77f0a7a7c966c0e77
Giteon <fox.gideon@gmail.com>
2024-04-24 00:33:09 -0700

    removed clam cursor code

--- classiccars.js
+++ classiccars.js
@@ -14,22 +14,3 @@ function carGenerator(){
     image.src = folder + thisCar['img'];
     caption.innerText = thisCar['name'];
 }
-
-// add clam cursor
-window.onload = function() {
-    console.log("Script loaded");  // Confirm the script is running
-
-    const openCursor = 'assets/small-clam.png';
-    const closedCursor = 'assets/small-clam-closed.png';
-    document.body.style.cursor = `url(${openCursor}), auto`;
-
-    document.body.addEventListener('mousedown', () => {
-        console.log("Mouse down");  // Confirm the event is triggered
-        document.body.style.cursor = `url(${closedCursor}), auto`;
-    });
-
-    document.body.addEventListener('mouseup', () => {
-        console.log("Mouse up");  // Confirm the event is triggered
-        document.body.style.cursor = `url(${openCursor}), auto`;
-    });
-};
