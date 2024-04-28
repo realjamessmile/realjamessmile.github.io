215d3df757048427603ae25a3f5972862e836967
realjamessmile <gosecurejim72@gmail.com>
2024-04-27 20:59:13 -0700

    Zappa time!

--- phish.js
+++ phish.js
@@ -1,14 +1,44 @@
+const zappaFreq = 0.5; // likelihood of Zappa time
+
 document.addEventListener('DOMContentLoaded', function() {
-    const audio = document.querySelector('#phish-audio'); 
     const phishButton = document.querySelector('#phish-button'); 
-    audio.volume = 0.3;
+    phishButton.classList.add('phish-button');
+
+    const audio = document.querySelector('#phish-audio');
+    const randn = Math.random();
+    let toggleText = '✋ STOP PHISHING ✋';
+    let vol = 0.25;
+    let zappaFlag = false;
+    if (randn > zappaFreq){
+        audio.src = "assets/phish.mp3";
+        audio.volume = vol;
+    }
+    else{
+        audio.src = "assets/zappa.mp3";
+        audio.volume = vol;
+        toggleText = 'WHOOPS! ZAPPA TIME';
+        zappaFlag = true;
+    }
+    
+    let clickCount = 0;
 
     // Function to toggle play/pause and update button text
     const togglePhish = () => {
         if (audio.paused) {  // If audio is paused, play it
             audio.play();
-            phishButton.textContent = '✋ STOP PHISHING ✋';  
-        } else {  // If audio is playing, pause it
+            phishButton.textContent = toggleText;
+            if (zappaFlag == true){
+                phishButton.classList.add('zappa-button');
+            }
+        }
+        else if (zappaFlag == true){
+            if (vol < 1){vol = vol * 2;}
+            else {audio.playbackRate = 1 + clickCount/5;}
+            audio.volume = vol;
+            phishButton.textContent = 'CANT STOP THE MUFFIN MAN!'
+            clickCount = clickCount + 1;
+        }
+        else {  // If audio is playing, pause it
             audio.pause();
             phishButton.textContent = '🎣 PHISH ME 🎣';  
         }
