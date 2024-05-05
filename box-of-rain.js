a32957fc8f485eb5726ebd07664ce72b9f8b7669
realjamessmile <gosecurejim72@gmail.com>
2024-05-04 20:38:35 -0700

    Ok maybe now jerry will play

--- box-of-rain.js
+++ box-of-rain.js
@@ -1,8 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
     const audio = document.querySelector('#box-audio');
-    let vol = 0;
+    let vol = 0.5;
     audio.volume = vol;
-    audio.play();
+
+    const playButton = document.querySelector('#play-button');
+    playButton.classList.add('play-button');
+
+    // Function to toggle play/pause and update button text
+    const togglePlay = () => {
+        if (audio.paused){
+            audio.play();
+            playButton.textContent = 'GO ON, JERRY';
+        }
+        else {
+            audio.pause();
+            playButton.textContent = 'HUSH NOW, JERRY';
+        }
+    }
+
+    playButton.addEventListener('click', togglePlay);
 
     let volume = document.getElementById('volume-slider');
     volume.addEventListener("change", function(e) {
